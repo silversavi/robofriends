188ffa7ff4db3a1ebf405abf788179e04f7c3a48
Silver Savi <silver.savi@gmail.com>
2019-08-22 23:43:34 +0300

    Updates

---
+++ precache-manifest.ed411f47280f4afbdad1726e8ea2e1c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b2d6726057ba032e12804ff73bff386",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13464a649aa1c96fd8b0",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "dae040ca91a9560a7987",
+    "url": "/static/css/main.3cbb8eaa.chunk.css"
+  },
+  {
+    "revision": "13464a649aa1c96fd8b0",
+    "url": "/static/js/2.541164eb.chunk.js"
+  },
+  {
+    "revision": "dae040ca91a9560a7987",
+    "url": "/static/js/main.474fd72b.chunk.js"
+  },
+  {
+    "revision": "b0a546bcc125fb21d6be",
+    "url": "/static/js/runtime~main.75271b63.js"
+  }
+]);
